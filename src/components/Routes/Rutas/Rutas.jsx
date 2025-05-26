770e5d6028c408787ecc3ec898dca87409a2b9dd
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-05-26 12:46:23 -0500

    ok

--- src/components/Routes/Rutas/Rutas.jsx
+++ src/components/Routes/Rutas/Rutas.jsx
@@ -5,11 +5,15 @@ import {Historia} from "../../pages/Historia/Historia"
 import {Introduccion} from "../../pages/Introduccion/Introduccion"
 import {Experiencia} from "../../pages/Experiencia/Experiencia"
 
+import { Menu } from '../../common/Menu/Menu'
+
 export function Rutas(){
     return(
 
         <>
+            <Menu />
             <Routes>
+                <Route path='/' element={<Home/>} ></Route>
                 <Route path='/inicio' element={<Home/>} ></Route>
                 <Route path='/proyectos' element={<Historia/>}></Route>
                 <Route path='/emelypacheco' element={<Introduccion/>}></Route>
@@ -18,4 +22,4 @@ export function Rutas(){
         </>
 
     )
-}
+}  
